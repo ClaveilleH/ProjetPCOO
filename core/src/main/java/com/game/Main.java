521a280fe0e989351c1d0c3e33601232c956b50d
Hugo <h.claveille@gmail.com>
2024-11-08 07:52:46 +0100

    ajout map et premier main qui l'affiche

--- core/src/main/java/com/game/Main.java
+++ core/src/main/java/com/game/Main.java
@@ -1,11 +1,61 @@
 package com.game;
 
 import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.maps.tiled.TiledMap;
+import com.badlogic.gdx.maps.tiled.TiledMapRenderer;
+import com.badlogic.gdx.maps.tiled.TmxMapLoader;
+import com.badlogic.gdx.maps.tiled.renderers.OrthoCachedTiledMapRenderer;
 
-/** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
+
 public class Main extends Game {
+//    Texture img;
+    TiledMap tiledMap;
+    OrthographicCamera camera;
+    TiledMapRenderer mapRenderer;
+    int x;
+
+
     @Override
     public void create() {
-        setScreen(new FirstScreen());
+        this.x = 0;
+
+        //setScreen(new FirstScreen());
+        float w = Gdx.graphics.getWidth();
+        float h = Gdx.graphics.getHeight();
+        System.out.println("w = " + w + ", h = " + h);
+        camera = new OrthographicCamera();
+        camera.setToOrtho(false, w, h);
+        camera.update();
+        System.out.println("Camera update");
+
+        tiledMap = loadMap();
+        System.out.println("Map loaded");
+        mapRenderer = new OrthoCachedTiledMapRenderer(tiledMap);
+        System.out.println("TiledMap loaded");
+
+        mapRenderer.setView(camera);
+    }
+
+    public TiledMap loadMap() {
+        return new TmxMapLoader().load("map/map1.tmx");
+    }
+
+    @Override
+    public void render() {
+        draw();
+
+    }
+
+
+    public void draw() {
+        camera.position.set(this.x, camera.viewportHeight / 2, 0);
+        camera.update();
+        this.x += 1;
+
+        mapRenderer.setView(camera);
+        System.out.println("Map rendered"+ this.x);
+        mapRenderer.render();
     }
 }
